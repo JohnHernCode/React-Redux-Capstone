578cbc9750264530ad27f23b176caa0273a794b5
JohnHernCode <besthostandseoinfo@gmail.com>
2021-07-30 12:42:11 -0600

    Add filter reducer

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,17 @@
 const ADD_POKEMON = 'ADD_POKEMON';
+const CHANGE_FILTER = 'CHANGE_FILTER';
 
 const addPokemon = pokemon => ({
   type: ADD_POKEMON,
   payload: pokemon,
 });
 
+const changeFilter = filter => ({
+  type: CHANGE_FILTER,
+  payload: filter,
+});
+
+
 export {
-  ADD_POKEMON, addPokemon,
+  ADD_POKEMON, addPokemon, CHANGE_FILTER, changeFilter,
 };
