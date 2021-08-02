7e1ddefee2ad744c150d7e2f3f50e18c262ea615
JohnHernCode <besthostandseoinfo@gmail.com>
2021-08-02 10:51:33 -0600

    Remove redux dev extention script

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import { createStore } from 'redux';
 import Routes from './routes/Routes';
 import rootReducer from './reducers/index'
 
-const store = createStore(rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer);
 
 function App() {
   return (
