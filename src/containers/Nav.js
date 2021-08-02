2498c8e14b68513ec76941d74f93c9da30882ad3
JohnHernCode <besthostandseoinfo@gmail.com>
2021-08-02 11:06:53 -0600

    Install material UI

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
       <>
-        <h2>Pokemon Pokédex</h2>
-        <div>
+        <span>
+          <h2>Pokemon Pokédex</h2>
           <Link to="/">Home </Link>
           <Link to="/about">About </Link>
-        </div>
+        </span>
       </>
   );
 }
